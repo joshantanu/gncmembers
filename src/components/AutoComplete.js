d7f1cad16b27e2cbbe0f5787bbac65c33297e6b6
Shantanu Joshi <joshantanu@gmail.com>
2020-09-21 23:26:23 +0530

    dropdown changes

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,79 +1,61 @@
-import React, {useState} from 'react';
-import {FormControl, Dropdown} from 'react-bootstrap';
+import React, { useState } from "react";
+import { FormControl, Dropdown } from "react-bootstrap";
 //import { deflate } from 'zlib';
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
-const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-    <a
-      href=""
-      ref={ref}
-      onClick={(e) => {
-        e.preventDefault();
-        onClick(e);
-      }}
-    >
-      {children}
-      &#x25bc;
-    </a>
-  ));
-  
-  // forwardRef again here!
-  // Dropdown needs access to the DOM of the Menu to measure it
-  const CustomMenu = React.forwardRef(
-    ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
-      const [value, setValue] = useState('');
-      console.log("selected Value=>", value)
-      return (
-        <div
-          ref={ref}
-          style={style}
-          className={className}
-          aria-labelledby={labeledBy}
-        >
-          <FormControl
-            autoFocus
-            className="mx-3 my-2 w-auto"
-            placeholder="Type to filter..."
-            onChange={(e) => setValue(e.target.value)}
-            value={value}
-          />
-          <ul className="list-unstyled">
-            { !!value && React.Children.toArray(children).filter(
-              (child) =>
-                !value || child.props.children.toLowerCase().indexOf(value)>=0,
-            )}
-          </ul>
-        </div>
-      );
-    },
+
+// forwardRef again here!
+// Dropdown needs access to the DOM of the Menu to measure it
+const AutoComplete = props => {
+  let [members, setMembers] = useState(props.membersData);
+  let [value, setValue] = useState("");
+  let [show, setShow] = useState(false);
+
+  const onchangeHandler = e => {
+    console.log(e.target.value);
+    let newVal = Object.keys(props.membersData).filter(
+      child =>
+        !e.target.value || child.toLowerCase().indexOf(e.target.value) >= 0
+    );
+
+    setValue(e.target.value);
+    setMembers(newVal);
+    setShow(true)
+  };
+
+  const onItemClick = e => {
+    console.log(e.target.value);
+  };
+
+  console.log("inautocomplete>>", props.membersData, value, members);
+  return (
+    <React.Fragment>
+    
+        <FormControl
+          autoFocus
+          className="mx-3 my-2 w-auto"
+          placeholder="Type to filter..."
+          onChange={onchangeHandler}
+          value={value}
+          autocomplete="off"
+        />
+      <Dropdown>
+        <Dropdown.Menu show={show}>
+          {members &&
+            members.map((key, index) => (
+              <Dropdown.Item
+                eventKey={index}
+                key={index}
+                value={props.membersData[key]}
+                
+              >
+                <span onClick={onItemClick}>{key}</span>
+              </Dropdown.Item>
+            ))}
+        </Dropdown.Menu>
+      </Dropdown>
+    </React.Fragment>
   );
-  
-  const AutoComplete = (props) => {
+};
 
-    console.log("inautocomplete>>",props.membersData)
-  return(
-    <Dropdown>
-      <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-        Custom toggle
-      </Dropdown.Toggle>
-  
-      <Dropdown.Menu as={CustomMenu}>
-        {/* <Dropdown.Item eventKey="1">Red</Dropdown.Item>
-        <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-        <Dropdown.Item eventKey="3" active>
-          Orange
-        </Dropdown.Item>
-        <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
-        */}
-      {props.membersData && Object.keys(props.membersData).map((key,index) =>
-        <Dropdown.Item eventKey={index} key={index} value={props.membersData[key]}>{key}</Dropdown.Item>
-        
-        )
-      }
-        </Dropdown.Menu> 
-    </Dropdown>
-  );
-
-  }
-
-  export default AutoComplete;
+export default AutoComplete;
